ddc141c539c971175e6f69aeb7bd293a5a9388cb
arash <arash@Arash.arash>
2024-06-14 16:43:44 +0330

    updated

--- models/models.py
+++ models/models.py
@@ -13,7 +13,7 @@ class SdMomMoms(models.Model):
 
 
     def _location_domain(self):
-        domain = [('1', '!=', 1)]
+        domain = [('id', '==', False)]
         partners = self.env['res.partner'].search([])
         partners = list([rec.id for rec in partners if rec.company_type == 'company'])
         if partners:
